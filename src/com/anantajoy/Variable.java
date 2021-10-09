cc0b5ceabac545f76f8517fddd4f5121b5eed645
ananta.xoy <anantajoy007@gmail.com>
2021-10-09 15:00:05 +0600

    basic java language syntax

---
+++ src/com/anantajoy/Variable.java
@@ -0,0 +1,16 @@
+package com.anantajoy;
+
+public class Variable {
+
+
+    public static void main(String[] var0) {
+        byte myAge = 21;
+        long viewsTime= 3_212_332_212L;
+        double weight = 65.5;
+        float distance = 20.4F;
+        char letter = 'A';
+        System.out.println(weight);
+        System.out.println(distance);
+        System.out.println(letter);
+    }
+}
